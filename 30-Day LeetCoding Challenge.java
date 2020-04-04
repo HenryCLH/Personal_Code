6c52c08e36b467d84c1748834bf5b68a7e27705b
HenryCLH <henrychenglh@gmail.com>
2020-04-03 23:05:19 -0400

    Week 1 : Happy Number

--- 30-Day LeetCoding Challenge.java
+++ 30-Day LeetCoding Challenge.java
@@ -15,3 +15,28 @@ class Solution {
 		return single;
     }
 }
+
+/* Happy Number */
+
+// Brute Calculate
+class Solution {
+    public boolean isHappy(int n)
+	{
+		int[] tmp = new int[730];
+		while (n != 1)
+		{
+			int t = 0;
+			while (n > 0)
+			{
+				t += (n % 10) * (n % 10);
+				n /= 10;
+			}
+			n = t;
+			if (tmp[n] == 0)
+				tmp[n] = 1;
+			else
+				return false;
+		}
+		return true;
+	}
+}
