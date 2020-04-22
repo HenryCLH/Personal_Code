06c8b43290ee8f89c5d5f06205e1f35060cc68f3
HenryCLH <henrychenglh@gmail.com>
2020-04-22 15:37:36 -0400

    Week 3 : Minimum Path Sum

--- 30-Day LeetCoding Challenge.java
+++ 30-Day LeetCoding Challenge.java
@@ -533,3 +533,32 @@ class Solution
 		return count;
 	}
 }
+
+/* Minimum Path Sum */
+// Greedy step by step
+class Solution
+{
+	public int minPathSum(int[][] grid)
+	{
+		int x = grid.length, y = grid[0].length;
+		for (int i = 0; i < x; i++)
+		{
+			for (int j = 0; j < y; j++)
+			{
+				if (i == 0)
+				{
+					if (j != 0)
+						grid[i][j] += grid[i][j - 1];
+				}
+				else
+				{
+					if (j == 0)
+						grid[i][j] += grid[i - 1][j];
+					else
+						grid[i][j] += (grid[i - 1][j] > grid[i][j - 1]) ? grid[i][j - 1] : grid[i - 1][j];
+				}
+			}
+		}
+		return grid[x - 1][y - 1];
+	}
+}
